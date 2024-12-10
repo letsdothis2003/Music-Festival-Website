87172d9b8aacdd44e43ffc281fefa2818a7ca73e
F.T <107659471+letsdothis2003@users.noreply.github.com>
2024-12-09 20:18:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './navbar';
-import Home from './home';
 import Contact from './contact';
 import TourLocations from './tourlocations';
 import BuyTickets from './buy-tickets';
@@ -14,7 +13,34 @@ function App() {
         <Navbar />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={
+              <div className="Home">
+                <h1>Welcome to Our Tour Website</h1>
+                <p>Explore amazing tour destinations and experiences!</p>
+
+                {/* Carl Wheezer content begins here */}
+                <h2>Fun Fact: Carl Wheezer - Singer Extraordinaire!</h2>
+                <p>
+                  Carl Wheezer, is known for his quirky personality
+                  and distinctive voice. But did you know that Carl is also a talented singer? While his musical
+                  talents aren't a major focus in the show, his unique vocal style and comedic performances have
+                  garnered attention. In fact, Carl's singing style is often humorous, but it has a charm all its own.
+                </p>
+                <p>
+                  Carl has also made a name for himself in internet culture, with many fans celebrating his high-pitched,
+                  earnest singing. From his renditions of *"Funky Town"* to his heartfelt performances of songs like
+                  *"Twinkle Twinkle Little Star"*, Carl Wheezer's musical contributions to the *Jimmy Neutron* universe
+                  are beloved by fans.
+                </p>
+                <p>
+                  Despite his somewhat neurotic and sometimes over-the-top persona, Carl's moments of singing have added
+                  an endearing quality to his character. So, next time you hear Carl belt out a song, remember: he’s
+                  not just the neurotic sidekick, he's also a singer with some real heart!
+                </p>
+                {/* End of Carl Wheezer content */}
+
+              </div>
+            } />
             <Route path="/contact" element={<Contact />} />
             <Route path="/tourlocations" element={<TourLocations />} />
             <Route path="/buytickets" element={<BuyTickets />} />
@@ -26,4 +52,3 @@ function App() {
 }
 
 export default App;
-
